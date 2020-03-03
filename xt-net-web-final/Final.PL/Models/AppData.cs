9a2fd7fd212aabfebff86326c423e7e49ddcc0e5
Yegor Kondratyev <evkondratyev@yandex.ru>
2020-03-03 11:26:06 +0400

    Added a header to the web page

---
+++ xt-net-web-final/Final.PL/Models/AppData.cs
@@ -0,0 +1,22 @@
+﻿using Final.BLL;
+using Final.BLL.Interfaces;
+using Final.DAL;
+using System.Web.Configuration;
+
+namespace Final.PL.Models
+{
+    public static class AppData
+    {
+        static AppData()
+        {
+            DaoCommon.ConnectionString = WebConfigurationManager.
+                ConnectionStrings["sqlDatabase"].ConnectionString;
+            UserLogic = new UserLogic(new UserDao());
+        }
+
+        public static IUserLogic UserLogic
+        {
+            get; private set;
+        }
+    }
+}
---
+++ xt-net-web-final/Final.PL/Models/AuthModel.cs
@@ -0,0 +1,57 @@
+﻿using Final.BLL.Interfaces;
+using Final.Entities;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Final.PL.Models
+{
+    public static class AuthModel
+    {
+        private static IUserLogic userLogic = AppData.UserLogic;
+
+        public static bool Authorize(string username, string password)
+        {
+            User entry = userLogic.GetByUsername(username);
+            if (entry == null)
+            {
+                return false;
+            }
+            using (var md5 = MD5.Create())
+            {
+                if (entry.PasswordHash == CreateHash(md5, password))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool Register(string username, string password)
+        {
+            if (userLogic.GetByUsername(username) != null)
+            {
+                return false;
+            }
+            User user = new User() {
+                Username = username
+            };
+            using (var md5 = MD5.Create())
+            {
+                user.PasswordHash = CreateHash(md5, password);
+            }
+            userLogic.Add(user);
+            return true;
+        }
+
+        public static string CreateHash(MD5 md5, string input)
+        {
+            byte[] data = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
+            var sBuilder = new StringBuilder();
+            for (var i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+            return sBuilder.ToString();
+        }
+    }
+}
