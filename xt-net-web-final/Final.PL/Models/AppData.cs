9450de75bf47cb64993e0149e0be8fe3d6148184
Yegor Kondratyev <evkondratyev@yandex.ru>
2020-03-08 22:51:40 +0400

    Added forum thread functionality on client side

--- xt-net-web-final/Final.PL/Models/AppData.cs
+++ xt-net-web-final/Final.PL/Models/AppData.cs
@@ -12,9 +12,15 @@ namespace Final.PL.Models
             DaoCommon.ConnectionString = WebConfigurationManager.
                 ConnectionStrings["sqlDatabase"].ConnectionString;
             UserLogic = new UserLogic(new UserDao());
+            ForumThreadLogic = new ForumThreadLogic(new ForumThreadDao());
         }
 
         public static IUserLogic UserLogic
+        {
+            get; private set;
+        }
+
+        public static IForumThreadLogic ForumThreadLogic
         {
             get; private set;
         }
--- xt-net-web-final/Final.PL/Models/AuthModel.cs
+++ xt-net-web-final/Final.PL/Models/AuthModel.cs
@@ -1,57 +1,23 @@
 ﻿using Final.BLL.Interfaces;
 using Final.Entities;
-using System.Security.Cryptography;
-using System.Text;
 
 namespace Final.PL.Models
 {
     public static class AuthModel
     {
-        private static IUserLogic userLogic = AppData.UserLogic;
+        private static readonly IUserLogic userLogic = AppData.UserLogic;
 
         public static bool Authorize(string username, string password)
         {
-            User entry = userLogic.GetByUsername(username);
-            if (entry == null)
-            {
-                return false;
-            }
-            using (var md5 = MD5.Create())
-            {
-                if (entry.PasswordHash == CreateHash(md5, password))
-                {
-                    return true;
-                }
-            }
-            return false;
+            return userLogic.CheckEntry(username, password);
         }
 
         public static bool Register(string username, string password)
         {
-            if (userLogic.GetByUsername(username) != null)
-            {
-                return false;
-            }
             User user = new User() {
                 Username = username
             };
-            using (var md5 = MD5.Create())
-            {
-                user.PasswordHash = CreateHash(md5, password);
-            }
-            userLogic.Add(user);
-            return true;
-        }
-
-        public static string CreateHash(MD5 md5, string input)
-        {
-            byte[] data = md5.ComputeHash(Encoding.UTF8.GetBytes(input));
-            var sBuilder = new StringBuilder();
-            for (var i = 0; i < data.Length; i++)
-            {
-                sBuilder.Append(data[i].ToString("x2"));
-            }
-            return sBuilder.ToString();
+            return userLogic.Add(user, password) != null;
         }
     }
 }
